d969bf2e0800e488d98d8a78b6e2634adca28f3f
Anna Kravets <annakravets1906@gmail.com>
2023-05-18 13:32:01 +0300

    Transferred to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,88 @@
 import { Component } from 'react';
 
-import Searchbar from './Searchbar/Searchbar';
-import ImageGallery from './ImageGallery/ImageGallery';
+import Searchbar from './Searchbar';
+import Loader from './Loader';
+import { getPictures } from '../services/getPictures';
+import ImageGallery from './ImageGallery';
+import Button from './Button';
 
 import { AppStyled } from './AppStyled';
 
 export default class App extends Component {
   state = {
     query: '',
+    loadMore: true,
+    pictures: [],
+    page: 1,
+    isLoading: false,
   };
 
   componentDidMount() {
     this.setState({ loading: true });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.setState({ isLoading: true, pictures: [] });
+      console.log(this.props.query);
+
+      getPictures(this.state.query, 1)
+        .then(res => res.json())
+        .then(newPictures =>
+          this.setState({
+            loadMore: true,
+            pictures: [...newPictures.hits],
+          })
+        )
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
+    }
+
+    if (
+      prevProps.query === this.props.query &&
+      prevState.page !== this.state.page
+    ) {
+      this.setState({ isLoading: true });
+
+      getPictures(this.props.query, this.state.page)
+        .then(res => res.json())
+
+        .then(newPictures =>
+          this.setState({
+            loadMore: true,
+            pictures: [...prevState.pictures, ...newPictures.hits],
+          })
+        )
+
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
+    }
+  }
+
   handleSearchFormSubmit = query => {
     this.setState({ query });
   };
 
+  handleCllickNextButton = page => {
+    this.setState({ page: this.state.page + 1 });
+  };
+
   render() {
-    const { query } = this.state;
+    const { pictures, isLoading, loadMore } = this.state;
+    console.log(pictures.length);
+    // if (pictures.length === 0 || pictures.length < 12) {
+    //   this.setState({ loadMore: false });
+    // }
 
     return (
       <AppStyled>
         <Searchbar onSubmit={this.handleSearchFormSubmit} />
-        <ImageGallery query={query} />
+        {isLoading && <Loader />}
+        <ImageGallery pictures={pictures} />
+        {loadMore && <Button onClick={this.handleCllickNextButton} />}
       </AppStyled>
     );
   }
 }
-
-//
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,105 +1,29 @@
-import { Component } from 'react';
-import PropTypes from 'prop-types';
-import { getPictures } from '../../services/getPictures';
+// import PropTypes from 'prop-types';
+
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
-import Loader from '../Loader/Loader';
-
 import { ImageGalleryList } from './ImageGalleryStyled';
-import Button from 'components/Button/Button';
 
-export default class ImageGallery extends Component {
-  state = {
-    pictures: [],
-    isLoading: false,
-    loadMore: false,
-    page: 1,
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.query !== this.props.query) {
-      this.setState({ isLoading: true, pictures: [], page: 1 });
-
-      getPictures(this.props.query, this.state.page)
-        .then(res => res.json())
-        .then(pictures => {
-          if (pictures.total === 0) {
-            this.setState({ loadMore: false });
-          } else {
-            return pictures;
-          }
-        })
-        .then(newPictures =>
-          this.setState({
-            loadMore: true,
-            pictures: [...prevState.pictures, ...newPictures.hits],
-          })
-        )
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
-    }
-    if (
-      prevProps.query === this.props.query &&
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ isLoading: true });
-
-      getPictures(this.props.query, this.state.page)
-        .then(res => res.json())
-
-        .then(newPictures =>
-          this.setState({
-            loadMore: true,
-            pictures: [...prevState.pictures, ...newPictures.hits],
-          })
-        )
-
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
-    }
-  }
-
-  handleCllickNextButton = page => {
-    this.setState({ page: this.state.page + 1 });
-  };
-
-  render() {
-    const { pictures, isLoading, loadMore } = this.state;
-
-    if (pictures.total === 0) {
-      return;
-    }
-
-    return (
-      <>
-        <ImageGalleryList>
-          {pictures &&
-            pictures.map(({ id, webformatURL, largeImageURL, tags }) => (
-              <>
-                <ImageGalleryItem
-                  key={id}
-                  img={webformatURL}
-                  largeImg={largeImageURL}
-                  tags={tags}
-                />
-              </>
-            ))}
-        </ImageGalleryList>
-        {isLoading && <Loader />}
-        {loadMore && <Button onClick={this.handleCllickNextButton} />}
-      </>
-    );
-  }
-}
-
-ImageGallery.propTypes = {
-  pictures: PropTypes.arrayOf(PropTypes.string),
-  id: PropTypes.number.isRequired,
-  img: PropTypes.string.isRequired,
-  largeImg: PropTypes.string.isRequired,
-  tags: PropTypes.string.isRequired,
-  isLoading: PropTypes.bool.isRequired,
-  loadMore: PropTypes.bool.isRequired,
-  page: PropTypes.number.isRequired,
+const ImageGallery = ({ pictures }) => {
+  return (
+    <ImageGalleryList>
+      {pictures.map(({ id, webformatURL, largeImageURL, tags }) => (
+        <ImageGalleryItem
+          key={id}
+          img={webformatURL}
+          largeImg={largeImageURL}
+          tags={tags}
+        />
+      ))}
+    </ImageGalleryList>
+  );
 };
+
+export default ImageGallery;
+
+// ImageGallery.propTypes = {
+//   pictures: PropTypes.arrayOf(PropTypes.string),
+//   id: PropTypes.number.isRequired,
+//   img: PropTypes.string.isRequired,
+//   largeImg: PropTypes.string.isRequired,
+//   tags: PropTypes.string.isRequired,
+// };
